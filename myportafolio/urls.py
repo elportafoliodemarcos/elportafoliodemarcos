6fb7a9de2946320784091476ee3b658d10bd6928
Marcos Enrique de Leòn Varela <mar1994cos@MacBook-Air-de-Marcos.local>
2026-01-06 15:57:21 +0100

    Fix: remove colaboracion URL causing 500 error

--- myportafolio/urls.py
+++ myportafolio/urls.py
@@ -17,8 +17,6 @@ urlpatterns += i18n_patterns(
     path('acerca/', views.acerca, name='acerca'),
     path('galeria/', views.galeria, name='galeria'),
     path('categoria/<slug:slug>/', views.categoria, name='categoria'),
-    path('colaboracion/', views.colaboracion, name='colaboracion'),
-    # path('colaboracion/', views.colaboracion, name='colaboracion'),
     path('contacto/', views.contacto, name='contacto'),
 )
 
