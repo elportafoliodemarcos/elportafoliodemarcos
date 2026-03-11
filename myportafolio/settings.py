3b2fb9c573128338522b5302aaa6f432063c1f83
Marcos Enrique de Leòn Varela <mar1994cos@MacBook-Air-de-Marcos.local>
2026-03-11 21:01:23 +0100

    database render

--- myportafolio/settings.py
+++ myportafolio/settings.py
@@ -77,13 +77,14 @@ TEMPLATES = [
 # -------------------------
 # DATABASE
 # -------------------------
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
+        conn_max_age=600
+    )
+}
 # -------------------------
 # I18N
 # -------------------------
